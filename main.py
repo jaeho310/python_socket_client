c2b2b9547511ced69ca82cfff121c9f1781172f9
jaeho choi <chlwogh1993@gmail.com>
2021-09-29 18:01:04 +0900

    update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import threading
 
 socket_client = None
 receive_thread = None
+command = {}
 
 def safety_exit():
   if socket_client:
@@ -21,19 +22,12 @@ def receive_message():
 
 def menu_user_interface():
   try:
-    print("############################################")
-    print("1. 데이터 읽기(get)")
-    print("2. 데이터 저장하기(put)")
-    print("3. 종료")
+    for _, key in enumerate(command):
+      print(command[key]['desc'])
     user_input = int(input())
-    if user_input == 1:
-      get_user_interface()
-    elif user_input == 2:
-      put_user_interface()
-    elif user_input == 3:
-      safety_exit()
-    else:
+    if user_input not in command:
       print("잘못 입력하였습니다.")
+    command[user_input]['func']()
     input()
   except Exception as e:
     if e.args[0] == 10054:
@@ -45,6 +39,9 @@ def get_user_interface():
   print("############################################")
   print("가져올 데이터의 key를 입력하세요")
   key = input()
+  if not key:
+    print("잘못 입력하였습니다.")
+    return
   parsed_data = 'get<' + key + '>'
   socket_client.send(parsed_data.encode())
 
@@ -52,23 +49,34 @@ def put_user_interface():
   print("############################################")
   print("저장할 데이터의 key를 입력하세요")
   key = input()
+  if not key:
+    print("잘못 입력하였습니다.")
+    return
   print("############################################")
   print("저장할 데이터의 값를 입력하세요")
   value = input()
+  if not value:
+    print("잘못 입력하였습니다.")
+    return
 
   parsed_data = 'put<' + key + ',' + value + '>'
   socket_client.send(parsed_data.encode())
+
+def command_pattern():
+  command[1] = {'desc': '1. 데이터 읽기(get)', 'func': get_user_interface}
+  command[2] = {'desc': '2. 데이터 저장하기(put)', 'func': put_user_interface}
+  command[3] = {'desc': '3. 종료', 'func': safety_exit}
 
 
 if __name__ == "__main__":
   print("##############소켓 클라이언트################")
   print("서버의 주소와 포트를 :로 구분하여 입력해주세요")
-  # print("############################################")
   try:
     ip, port = input().split(":")
     socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_client.connect((ip, int(port)))
     receive_thread = threading.Thread(target = receive_message, args=())
+    command_pattern()
     receive_thread.start()
     while True:
       menu_user_interface()
